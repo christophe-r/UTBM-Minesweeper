b94422d0b7d875da7e60c87edfa371ed50443941
christophe-r <contact@christophe-ribot.fr>
2014-04-29 23:05:07 +0200

    Revert "About window, Option window"
    This reverts commit 260b279165e45b0d00ce1492dd64f577e5a11bb9.

--- UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/AboutWindow.java
+++ UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/AboutWindow.java
@@ -1,27 +1,14 @@
 package fr.utbm.lp24.minesweeper;
 
-import java.awt.Desktop;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
-
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 
 /**
  * @author Christophe
@@ -39,7 +26,7 @@ public class AboutWindow extends JDialog implements ActionListener {
 	public AboutWindow(){
 
 		this.setTitle("About UTBM Minesweeper");
-		this.setSize(360, 286);
+		this.setSize(300, 200);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		this.setLocationRelativeTo(null);
@@ -51,73 +38,12 @@ public class AboutWindow extends JDialog implements ActionListener {
 		 
 		 
 			JPanel panel = new JPanel();
+			panel.setLayout(new FlowLayout());
 			boutonOk = new JButton("OK");
-			boutonOk.setBounds(264, 225, 80, 23);
 			boutonOk.addActionListener(this);
-			panel.setLayout(null);
 			panel.add(boutonOk);
 			
-			getContentPane().add(panel);
-			
-			JPanel panel_1 = new JPanel(){
-
-				private static final long serialVersionUID = 1L;
-
-				public void paintComponent(Graphics g) {  
-					Image img = Toolkit.getDefaultToolkit().getImage(
-					AboutWindow.class.getResource("/resources/icon64.png"));
-					g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
-				}
-				
-			};
-			panel_1.setBounds(20, 20, 64, 64);
-			panel.add(panel_1);
-			
-			JLabel lblUtbmMinesweeper = new JLabel("UTBM Minesweeper");
-			lblUtbmMinesweeper.setForeground(new Color(128, 128, 128));
-			lblUtbmMinesweeper.setFont(new Font("Tahoma", Font.BOLD, 24));
-			lblUtbmMinesweeper.setBounds(94, 18, 250, 29);
-			panel.add(lblUtbmMinesweeper);
-			
-			JLabel lblNewLabel = new JLabel("<html>This Java application \"Minesweeper\" was created in spring 2014 at the University of Technology of Belfort-Montb\u00E9liard, within the course credit LP24.");
-			lblNewLabel.setBounds(20, 95, 324, 42);
-			panel.add(lblNewLabel);
-			
-			JLabel lblNewLabel_1 = new JLabel("Authors:");
-			lblNewLabel_1.setBounds(20, 148, 52, 14);
-			panel.add(lblNewLabel_1);
-			
-			JLabel lblVincentMarseguerra = new JLabel("Vincent MARSEGUERRA");
-			lblVincentMarseguerra.setBounds(71, 148, 130, 14);
-			panel.add(lblVincentMarseguerra);
-			
-			JLabel lblChristopheRibotwwwchristopheribotfr = new JLabel("Christophe RIBOT (www.christophe-ribot.fr)");
-			lblChristopheRibotwwwchristopheribotfr.setBounds(71, 162, 225, 14);
-			panel.add(lblChristopheRibotwwwchristopheribotfr);
-			
-			JLabel lblGithubUrl = new JLabel("GitHub page:");
-			lblGithubUrl.setBounds(20, 196, 64, 14);
-			panel.add(lblGithubUrl);
-			
-			JLabel lblNewLabel_2 = new JLabel("<html><u>https://github.com/christophe-r/UTBM-Minesweeper</u>");
-			lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
-			lblNewLabel_2.setForeground(new Color(0, 0, 255));
-			lblNewLabel_2.addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseClicked(MouseEvent arg0) {
-					Desktop desktop = Desktop.getDesktop();
-					try {
-	                    URI uri = new URI("https://github.com/christophe-r/UTBM-Minesweeper");
-	                    desktop.browse(uri);
-	                } catch (IOException ex) {
-	                    ex.printStackTrace();
-	                } catch (URISyntaxException ex) {
-	                    ex.printStackTrace();
-	                }
-				}
-			});
-			lblNewLabel_2.setBounds(90, 196, 253, 14);
-			panel.add(lblNewLabel_2);
+			this.add(panel);
 
 		
 		this.setVisible(true);
@@ -132,4 +58,6 @@ public class AboutWindow extends JDialog implements ActionListener {
 			this.setVisible(false); // Quit the About window
 		}
 	}
+	
+
 }
