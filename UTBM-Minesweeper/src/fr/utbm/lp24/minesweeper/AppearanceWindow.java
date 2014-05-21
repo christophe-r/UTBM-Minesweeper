c5154b45f6b86419d0570be5901f620734059748
Christophe R. <contact@christophe-ribot.fr>
2014-05-21 14:09:33 +0200

    Add theme preview image in Appearance Window

--- UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/AppearanceWindow.java
+++ UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/AppearanceWindow.java
@@ -1,6 +1,9 @@
 package fr.utbm.lp24.minesweeper;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,6 +15,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.ButtonGroup;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AppearanceWindow extends JDialog {
 
@@ -34,7 +39,7 @@ public class AppearanceWindow extends JDialog {
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setResizable(false);
 		setTitle("Change Appearance");
-		setBounds(100, 100, 550, 255);
+		setBounds(100, 100, 300, 420);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -46,7 +51,7 @@ public class AppearanceWindow extends JDialog {
 					appearanceWindowOK();
 				}
 			});
-			okButton.setBounds(189, 194, 100, 23);
+			okButton.setBounds(72, 357, 100, 23);
 			contentPanel.add(okButton);
 			okButton.setActionCommand("OK");
 			getRootPane().setDefaultButton(okButton);
@@ -58,30 +63,118 @@ public class AppearanceWindow extends JDialog {
 					setVisible(false);
 				}
 			});
-			cancelButton.setBounds(299, 194, 100, 23);
+			cancelButton.setBounds(182, 357, 100, 23);
 			contentPanel.add(cancelButton);
 			cancelButton.setActionCommand("Cancel");
 		}
 
 		rdbtnWin7Classic = new JRadioButton("Windows 7 Classic");
 		buttonGroup.add(rdbtnWin7Classic);
-		rdbtnWin7Classic.setBounds(20, 139, 125, 23);
+		rdbtnWin7Classic.setBounds(20, 139, 113, 23);
+		rdbtnWin7Classic.setFocusPainted(false);
 		contentPanel.add(rdbtnWin7Classic);
 
 		rdbtnWin7Flower = new JRadioButton("Windows 7 Flower");
 		buttonGroup.add(rdbtnWin7Flower);
-		rdbtnWin7Flower.setBounds(147, 139, 125, 23);
+		rdbtnWin7Flower.setBounds(159, 139, 113, 23);
+		rdbtnWin7Flower.setFocusPainted(false);
 		contentPanel.add(rdbtnWin7Flower);
 
 		rdbtnWin98Classic = new JRadioButton("Windows 98 Classic");
 		buttonGroup.add(rdbtnWin98Classic);
-		rdbtnWin98Classic.setBounds(274, 139, 125, 23);
+		rdbtnWin98Classic.setBounds(20, 305, 119, 23);
+		rdbtnWin98Classic.setFocusPainted(false);
 		contentPanel.add(rdbtnWin98Classic);
-		
+
 		rdbtnWin8Classic = new JRadioButton("Windows 8 Classic");
 		buttonGroup.add(rdbtnWin8Classic);
-		rdbtnWin8Classic.setBounds(408, 139, 125, 23);
+		rdbtnWin8Classic.setBounds(157, 305, 113, 23);
+		rdbtnWin8Classic.setFocusPainted(false);
 		contentPanel.add(rdbtnWin8Classic);
+
+
+
+		JPanel preview_win7classic = new JPanel(){
+
+			private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g) {  
+				Image img = Toolkit.getDefaultToolkit().getImage(
+						AppearanceWindow.class.getResource("/resources/themes/win7_classic/preview.png"));
+				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
+			}
+
+		};
+		preview_win7classic.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				rdbtnWin7Classic.doClick();
+			}
+		});
+		preview_win7classic.setBounds(20, 20, 110, 110);
+		contentPanel.add(preview_win7classic);
+
+		JPanel preview_win7flower = new JPanel(){
+
+			private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g) {  
+				Image img = Toolkit.getDefaultToolkit().getImage(
+						AppearanceWindow.class.getResource("/resources/themes/win7_flower/preview.png"));
+				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
+			}
+
+		};
+		preview_win7flower.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				rdbtnWin7Flower.doClick();
+			}
+		});
+		preview_win7flower.setBounds(160, 20, 110, 110);
+		contentPanel.add(preview_win7flower);
+
+		JPanel preview_win98classic = new JPanel(){
+
+			private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g) {  
+				Image img = Toolkit.getDefaultToolkit().getImage(
+						AppearanceWindow.class.getResource("/resources/themes/win98_classic/preview.png"));
+				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
+			}
+
+		};
+		preview_win98classic.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				rdbtnWin98Classic.doClick();
+				//rdbtnWin98Classic.setFocusPainted(false);
+
+			}
+		});
+		preview_win98classic.setBounds(20, 188, 110, 110);
+		contentPanel.add(preview_win98classic);
+
+		JPanel preview_win8classic = new JPanel(){
+
+			private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g) {  
+				Image img = Toolkit.getDefaultToolkit().getImage(
+						AppearanceWindow.class.getResource("/resources/themes/win8_classic/preview.png"));
+				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
+			}
+
+		};
+		preview_win8classic.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				rdbtnWin8Classic.doClick();
+			}
+		});
+		preview_win8classic.setBounds(159, 188, 110, 110);
+		contentPanel.add(preview_win8classic);
 
 
 		appearanceWindowInit();
@@ -138,6 +231,4 @@ public class AppearanceWindow extends JDialog {
 		setVisible(false);
 
 	}
-
-
 }
