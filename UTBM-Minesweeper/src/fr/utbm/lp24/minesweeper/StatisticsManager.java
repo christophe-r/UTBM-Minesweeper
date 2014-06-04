b84ba65b64a63d781090cc0e47e32f05b40e89bb
Christophe R. <contact@christophe-ribot.fr>
2014-06-04 22:34:27 +0200

    StatisticsWindow improvements & StatisticsManager

--- UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/StatisticsManager.java
+++ UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/StatisticsManager.java
@@ -1,25 +1,99 @@
 package fr.utbm.lp24.minesweeper;
 
-public class StatisticsManager extends PreferencesManager{
+/**
+ * Class to manage the statistics
+ * @author Christophe and Vincent
+ *
+ */
+public class StatisticsManager extends PreferencesManager {
 
+	/**
+	 * Add time to the total time counter
+	 * @param time
+	 */
 	public void addTime(int time){
-		int oldtime = Integer.parseInt(getPref("Stat_Time","0"));
-		oldtime += time;
-		setPref("Stat_Time", oldtime + "");
+		int oldTime = Integer.parseInt(getPref("stats_total_time", "0"));
+		oldTime += time;
+		setPref("stats_total_time", oldTime + "");
 	}
+
+	/**
+	 * Add game to the total counter
+	 */
 	public void addGamePlayed(){
-		int number = Integer.parseInt(getPref("Stat_Played","0"));
-		number ++;
-		setPref("Stat_Time",number + "");
+		int gamesPlayed = Integer.parseInt(getPref("stats_games_played", "0"));
+		gamesPlayed++;
+		setPref("stats_games_played", gamesPlayed + "");
 	}
+
+	/**
+	 * Add won game to the total counter
+	 */
 	public void addGameWon(){
-		int number = Integer.parseInt(getPref("Stat_Won","0"));
-		number ++;
-		setPref("Stat_Time", number + "");
+		int gamesWon = Integer.parseInt(getPref("stats_games_won", "0"));
+		gamesWon++;
+		setPref("stats_games_won", gamesWon + "");
 	}
+
+	/**
+	 * Save the score if it's the best
+	 * @param score
+	 */
 	public void testBestScore(int score){
-		if(Integer.parseInt(getPref("Stat_Score","0")) < score){
-			setPref("Stat_Score", score + "");
+		if( Integer.parseInt(getPref("stats_best_score", "0")) < score ){
+			setPref("stats_best_score", score + "");
 		}
 	}
+
+	/**
+	 * Get the total time played
+	 * @return HH:MM:SS
+	 */
+	public String getTotalTimePlayed(){
+
+		int seconds = Integer.parseInt(getPref("stats_total_time", "0"));
+		int hours = (int) seconds / 3600;
+		int remainder = (int) seconds - hours * 3600;
+		int mins = remainder / 60;
+		remainder = remainder - mins * 60;
+		int secs = remainder;
+
+		return ((hours<10)?"0"+hours:hours)+":"+((mins<10)?"0"+mins:mins)+":"+((secs<10)?"0"+secs:secs);
+	}
+
+	/**
+	 * Get the total games played
+	 * @return total games played
+	 */
+	public int getGamesPlayed(){
+		return Integer.parseInt(getPref("stats_games_played", "0"));
+	}
+
+	/**
+	 * Get the total games won
+	 * @return total games won
+	 */
+	public int getGamesWon(){
+		return Integer.parseInt(getPref("stats_games_won", "0"));
+	}
+
+	/**
+	 * Get the best score
+	 * @return best score
+	 */
+	public int getBestScore(){
+		return Integer.parseInt(getPref("stats_best_score", "0"));
+	}
+
+	/**
+	 * Reset the statistics
+	 */
+	public void reset(){
+		setPref("stats_total_time", "0");
+		setPref("stats_games_played", "0");
+		setPref("stats_games_won", "0");
+		setPref("stats_best_score", "0");
+	}
+
+
 }
--- UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/StatisticsWindow.java
+++ UTBM-Minesweeper/src/fr/utbm/lp24/minesweeper/StatisticsWindow.java
@@ -15,10 +15,14 @@ public class StatisticsWindow extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 
+	private JLayeredPane layeredPane_localStats;
 
-	/**
-	 * Create the dialog.
-	 */
+	private JLabel label_totalTime;
+	private JLabel label_gamesPlayed;
+	private JLabel label_gamesWon;
+	private JLabel label_winPercentage;
+	private JLabel label_bestScore;
+
 	public StatisticsWindow() {
 		setResizable(false);
 		setTitle("Statistics");
@@ -36,7 +40,7 @@ public class StatisticsWindow extends JDialog {
 					setVisible(false);
 				}
 			});
-			okButton.setBounds(127, 277, 90, 23);
+			okButton.setBounds(164, 277, 90, 23);
 			getContentPane().add(okButton);
 			okButton.setActionCommand("OK");
 			getRootPane().setDefaultButton(okButton);
@@ -46,65 +50,84 @@ public class StatisticsWindow extends JDialog {
 		tabbedPane.setBounds(10, 11, 244, 255);
 		getContentPane().add(tabbedPane);
 
-		JLayeredPane layeredPane_localStats = new JLayeredPane();
+
+		/************* Stats tab ******************/
+
+		layeredPane_localStats = new JLayeredPane();
 		tabbedPane.addTab("Local statistics", null, layeredPane_localStats, null);
 		layeredPane_localStats.setLayout(null);
 
 		JLabel totalTime = new JLabel("Total time:");
 		totalTime.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		totalTime.setBounds(30, 30, 82, 20);
+		totalTime.setBounds(20, 29, 82, 20);
 		layeredPane_localStats.add(totalTime);
 
 		JLabel gamePlayed = new JLabel("Games played:");
 		gamePlayed.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		gamePlayed.setBounds(30, 61, 129, 20);
+		gamePlayed.setBounds(20, 60, 129, 20);
 		layeredPane_localStats.add(gamePlayed);
 
 		JLabel gamesWon = new JLabel("Games won:");
 		gamesWon.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		gamesWon.setBounds(30, 92, 115, 20);
+		gamesWon.setBounds(20, 90, 115, 20);
 		layeredPane_localStats.add(gamesWon);
 
 		JLabel winPercentage = new JLabel("Win percentage:");
 		winPercentage.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		winPercentage.setBounds(30, 123, 115, 20);
+		winPercentage.setBounds(20, 120, 115, 20);
 		layeredPane_localStats.add(winPercentage);
 
 		JLabel bestScore = new JLabel("Best score:");
 		bestScore.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		bestScore.setBounds(30, 154, 115, 20);
+		bestScore.setBounds(20, 150, 115, 20);
 		layeredPane_localStats.add(bestScore);
 
-		JLabel label_totalTime = new JLabel("0");
+		label_totalTime = new JLabel("00:00:00");
 		label_totalTime.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_totalTime.setBounds(169, 30, 46, 20);
+		label_totalTime.setBounds(159, 29, 70, 20);
 		layeredPane_localStats.add(label_totalTime);
 
-		JLabel label_gamesPlayed = new JLabel("0");
+		label_gamesPlayed = new JLabel("0");
 		label_gamesPlayed.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_gamesPlayed.setBounds(169, 64, 46, 17);
+		label_gamesPlayed.setBounds(159, 60, 70, 20);
 		layeredPane_localStats.add(label_gamesPlayed);
 
-		JLabel label_gamesWon = new JLabel("0");
+		label_gamesWon = new JLabel("0");
 		label_gamesWon.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_gamesWon.setBounds(169, 95, 46, 20);
+		label_gamesWon.setBounds(159, 90, 70, 20);
 		layeredPane_localStats.add(label_gamesWon);
 
-		JLabel label_winPercentage = new JLabel("0");
+		label_winPercentage = new JLabel("0 %");
 		label_winPercentage.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_winPercentage.setBounds(169, 126, 46, 20);
+		label_winPercentage.setBounds(159, 120, 70, 20);
 		layeredPane_localStats.add(label_winPercentage);
 
-		JLabel label_bestScore = new JLabel("0");
+		label_bestScore = new JLabel("0");
 		label_bestScore.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_bestScore.setBounds(169, 157, 46, 17);
+		label_bestScore.setBounds(159, 150, 70, 20);
 		layeredPane_localStats.add(label_bestScore);
+
+		updateStatistics();
+
+		JButton btnReset = new JButton("Reset");
+		btnReset.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				StatisticsManager playerStats = new StatisticsManager();
+				playerStats.reset();
+				updateStatistics();
+			}
+		});
+
+		btnReset.setBounds(88, 193, 61, 23);
+		layeredPane_localStats.add(btnReset);
+
+
+		/************** Ranking tab ***************/
 
 		JLayeredPane layeredPane_globalRanking = new JLayeredPane();
 		tabbedPane.addTab("Global ranking", null, layeredPane_globalRanking, null);
 		layeredPane_globalRanking.setLayout(null);
-
-
 
 		JLabel rank = new JLabel("#");
 		rank.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -126,8 +149,6 @@ public class StatisticsWindow extends JDialog {
 		score.setBounds(185, 10, 44, 14);
 		layeredPane_globalRanking.add(score);
 
-
-
 		ArrayList<ArrayList<String>> globalScore = new ScoreManager().getScores();
 
 		int y = 30;
@@ -138,7 +159,6 @@ public class StatisticsWindow extends JDialog {
 				JLabel label = new JLabel(""+field);
 				label.setBounds(x, y, 60, 14);
 				layeredPane_globalRanking.add(label);
-
 
 				switch(nbCol){
 				case 0: x += 20; break;
@@ -152,8 +172,32 @@ public class StatisticsWindow extends JDialog {
 			y += 20;
 		}
 
-
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+
+
+
+
+	private void updateStatistics(){
+		StatisticsManager playerStats = new StatisticsManager();
+
+		label_totalTime.setText(playerStats.getTotalTimePlayed());
+		label_gamesPlayed.setText(playerStats.getGamesPlayed()+"");
+		label_gamesWon.setText(playerStats.getGamesWon()+"");
+
+		float winPercentage_value = 0;
+
+		if( playerStats.getGamesPlayed() != 0 ){
+			winPercentage_value = (float)Math.round((float)playerStats.getGamesWon()/playerStats.getGamesPlayed()*100*10)/10;
+		}
+
+		label_winPercentage.setText(winPercentage_value+" %");
+		label_bestScore.setText(playerStats.getBestScore()+"");
+
+	}
+
+
+
+
 }
